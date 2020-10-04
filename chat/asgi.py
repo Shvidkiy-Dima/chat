d8c97ea8920310bab3603488d75714749872ea72
Dima <shvidkiy.dmitriy@gmail.com>
2020-10-04 22:55:18 +0300

    fix asgi.py

--- chat/asgi.py
+++ chat/asgi.py
@@ -6,11 +6,10 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
-
 import os
+import django
+from channels.routing import get_default_application
 
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat.settings')
-
-application = get_asgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'chat.settings')
+django.setup()
+application = get_default_application()
